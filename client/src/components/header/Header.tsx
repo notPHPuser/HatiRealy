37a31ef90939357055c3e56f8e72a534acc69a78
notPHPuser <artem2005dm@mail.ru>
2024-07-19 14:10:58 +0300

    надо разбираться так как пропадает текст

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import styles from './header.module.css';
 import { Link } from 'react-router-dom';
 import { Header as HeaderInterface } from '../../interface/headerInterface'; //
 import RegOrProfile from './RegOrProfile';
+import { Bell, Heart, MessageSquareText, Search } from 'lucide-react';
 
 export default function Header() {
   const header: HeaderInterface = {
@@ -21,28 +22,33 @@ export default function Header() {
         </Link>
         <div className={styles.for_photo}>
           <Link className={styles.Link_header_photo} to='/chat'>
-            <img src='/chat.png' alt='chat' className={styles.photo_header} />
+            <MessageSquareText color='black' className={styles.photo_header} />
+            {/* <img src='/chat.png' alt='chat' className={styles.photo_header} /> */}
+            {/* надо разбираться так как пропадает иконка*/}
             <div className={`${styles.text_for_link} ${styles.hidden}`}>
               <p className={styles.text_for_text}> {header.messages}</p>{' '}
             </div>
           </Link>
 
           <Link className={styles.Link_header_photo} to='/like'>
-            <img src='/like.png' alt='like' className={styles.photo_header} />
+            {/* <img src='/like.png' alt='like' className={styles.photo_header} /> */}
+            <Heart />
             <div className={`${styles.text_for_link} ${styles.hidden}`}>
               <p className={styles.text_for_text}>{header.likes}</p>{' '}
             </div>
           </Link>
 
           <Link className={styles.Link_header_photo} to='/save_find'>
-            <img src='/save_find.png' alt='save_find' className={styles.photo_header} />
+            {/* <img src='/save_find.png' alt='save_find' className={styles.photo_header} /> */}
+            <Search />
             <div className={`${styles.text_for_link} ${styles.hidden}`}>
               <p className={styles.text_for_text}>{header.saves}</p>
             </div>
           </Link>
 
           <Link className={styles.Link_header_photo} to='/notification'>
-            <img src='/notification.png' alt='notification' className={styles.photo_header} />
+            {/* <img src='/notification.png' alt='notification' className={styles.photo_header} /> */}
+            <Bell />
             <div className={`${styles.text_for_link} ${styles.hidden}`}>
               <p className={styles.text_for_text}>{header.notifications}</p>
             </div>
