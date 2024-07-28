3a4387757fea22bcfa64f2fcd7aa2d1119c0cd05
notPHPuser <artem2005dm@mail.ru>
2024-07-28 15:06:21 +0300

    done marg

--- client/src/components/header/RegOrProfile.tsx
+++ client/src/components/header/RegOrProfile.tsx
@@ -6,7 +6,7 @@ export default function RegOrProfile() {
     <>
       <div className={styles.reg_or_profile}>
         <button className={`${styles.button_for_reg} ${styles.first}`}>
-          <p className={styles.p_for_reg}> + Подать за 0 руб</p>{' '}
+          <p className={styles.p_for_reg}> + Подать за 0 руб</p>
         </button>
         <button className={`${styles.button_for_reg} ${styles.second}`}>
           <p className={styles.p_for_reg}> Войти</p>
